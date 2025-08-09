2fc4afc514ee21a7d7b016139e6bb8cb1f52e2ef
madiaz-affin <154223982+miguel-angel-diaz@users.noreply.github.com>
2025-08-09 08:56:36 +0200

    Update discord permissions

--- main.py
+++ main.py
@@ -31,54 +31,41 @@ def comando_roles_permitidos(*roles):
 
 @bot.command(name="strike")
 @comando_roles_permitidos("admin")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
-@enviar_ayuda_handle()
 async def strike(ctx, miembro: discord.Member = None):
     """Aplica un strike a un miembro del servidor - !strike <usuario>"""
     await aplicar_strike(ctx, miembro)
 
 @bot.command(name="out")
 @comando_roles_permitidos("admin")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def out(ctx, miembro: discord.Member = None):
     """aplica el rol 'Out' a un miembro del servidor - !out <usuario>"""
     await aplicar_out(ctx, miembro)
 
 @bot.command(name="eliminar-mensajes")
 @comando_roles_permitidos("admin")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def clear(ctx, canal: discord.TextChannel = None, cantidad: int = None):
     """Elimina una cantidad específica de mensajes en un canal - !eliminar-mensajes <canal> <cantidad>"""
     await eliminar_mensajes(ctx, canal, cantidad)
 
 @bot.command(name="cerrar-peticion")
 @comando_roles_permitidos("admin")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def cerrar_peticion(ctx, codigo: str = None, *, respuesta: str = None):
     """Cierra una petición y envía la respuesta al usuario - !cerrar-peticion <código> <respuesta>"""
     await cerrar_peticion_handle(ctx, codigo, respuesta)
 
 @bot.command(name="sorteo-torneo")
 @comando_roles_permitidos("admin")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def sorteo_torneo(ctx, codigo_torneo: str, *, premio: str = "Premio del sorteo"):
     """Realiza un sorteo entre los inscritos de un torneo - !sorteo-torneo <código_torneo> <premio>"""
     await sorteo_torneo_handle(ctx, codigo_torneo, premio)
 
 @bot.command(name="nuevo-sorteo")
 @comando_roles_permitidos("admin")
-@commands.has_permissions(manage_guild=True)
 async def nuevo_sorteo(ctx, *, args: str = None):
     await nuevo_sorteo_handle(ctx, args=args)
 
 @bot.command(name="realizar-sorteo")
 @comando_roles_permitidos("admin")
-@commands.has_permissions(manage_guild=True)
 async def realizar_sorteo(ctx, codigo: str):
     await realizar_sorteo_handle(ctx, codigo.strip())
 
@@ -90,72 +77,54 @@ async def realizar_sorteo(ctx, codigo: str):
 
 @bot.command(name="agendar-partida")
 @comando_roles_permitidos("socio", "second-chance-socio", "miembro", "second-chance-miembro")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def agendar_partida(ctx, fecha=None, hora=None, jugador1: discord.Member = None, _vs=None, jugador2: discord.Member = None):
     """Agenda una partida entre dos jugadores - !agendar-partida <fecha> <hora> <jugador1> vs <jugador2>"""
     await agendar_partida_handle(ctx, fecha, hora, jugador1, _vs, jugador2)
 
 @bot.command(name="eventos-hoy")
 @comando_roles_permitidos("socio", "second-chance-socio", "miembro", "second-chance-miembro")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def eventos_hoy(ctx):
     """Muestra los eventos programados para hoy - !eventos-hoy"""
     await eventos_hoy_handle(ctx)
 
 @bot.command(name="nueva-peticion")
 @comando_roles_permitidos("socio", "second-chance-socio", "miembro", "second-chance-miembro")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def nueva_peticion(ctx, *, descripcion: str = None):
     """Crea una nueva petición - !nueva-peticion <descripción>"""
     await nueva_peticion_handle(ctx, descripcion)
 
 @bot.command(name="inscribirse")
 @comando_roles_permitidos("socio", "second-chance-socio", "miembro", "second-chance-miembro")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def inscribirse(ctx, codigo: str = None, usuario: discord.Member = None):
     """Inscribe a un usuario en un torneo - !inscribirse <código> (<usuario> solo Administradores)"""
     await inscribirse_handler(ctx, codigo, usuario)
 
 @bot.command(name="desinscribirse")
 @comando_roles_permitidos("socio", "second-chance-socio", "miembro", "second-chance-miembro")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def desinscribirse(ctx, codigo: str = None, usuario: discord.Member = None):
     """Desinscribe a un usuario de un torneo - !desinscribirse <código> (<usuario> solo Administradores)"""
     await desinscribirse_handler(ctx, codigo, usuario)
 
 @bot.command(name="ver-inscritos")
 @comando_roles_permitidos("socio", "second-chance-socio", "miembro", "second-chance-miembro")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def ver_inscritos(ctx, codigo=None):
     """Muestra los inscritos en un torneo - !ver-inscritos <código>"""
     await ver_inscritos_handler(ctx, codigo)
 
 @bot.command(name="reportar-resultado")
 @comando_roles_permitidos("socio", "second-chance-socio", "miembro", "second-chance-miembro")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def reportar_resultado(ctx, codigo_torneo: str, jugador1: discord.Member, resultado: str, jugador2: discord.Member):
     """Reporta el resultado de un partido de un torneo - !reportar-resultado <código_torneo> <jugador1> <resultado> <jugador2>"""
     await reportar_resultado_handle(ctx, codigo_torneo, jugador1, resultado, jugador2)
 
 @bot.command(name="partidos-pendientes")
 @comando_roles_permitidos("socio", "second-chance-socio", "miembro", "second-chance-miembro")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def partidos_pendientes(ctx, codigo_torneo: str, type='user'):
     """Muestra los partidos pendientes de esa ronda de un torneo - !partidos-pendientes <código_torneo>"""
     await partidos_pendientes_handle(ctx, codigo_torneo, type)
 
 @bot.command(name="inscribirse-sorteo")
 @comando_roles_permitidos("socio", "second-chance-socio", "miembro", "second-chance-miembro")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def inscribirse_sorteo(ctx, codigo: str):
     await inscribirse_sorteo_handle(ctx, codigo)
 
@@ -167,24 +136,18 @@ async def inscribirse_sorteo(ctx, codigo: str):
 
 @bot.command(name="nuevo-torneo")
 @comando_roles_permitidos("admin")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def new_tournament(ctx, *, args=None):
     """Crea un nuevo torneo en Challonge -!nuevo-torneo Nombre | Formato | tipo | Jugadores | Fecha | Roles_permitidos | DeckURL"""
     await new_tournament_assistance_handle(ctx, args=args)
 
 @bot.command(name="iniciar-torneo")
 @comando_roles_permitidos("admin")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def iniciar_torneo(ctx, codigo_torneo: str):
     """Inicia un torneo con el código proporcionado - !iniciar-torneo <código_torneo>"""
     await iniciar_torneo_handle(ctx, codigo_torneo)
 
 @bot.command(name="actualizar-clasificacion")
 @comando_roles_permitidos("admin")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def actualizar_clasificacion(ctx, codigo_torneo: str):
     """Actualiza la clasificación con criterios estilo MTG y la publica en #clasificaciones-torneos - !actualizar-clasificacion <código_torneo>"""
     await borrar_mensaje_seguro(ctx)
@@ -197,8 +160,6 @@ async def actualizar_clasificacion(ctx, codigo_torneo: str):
 
 @bot.command(name="forzar-ronda")
 @comando_roles_permitidos("admin")
-@commands.has_permissions(manage_messages=True)
-@commands.has_permissions(manage_roles=True)
 async def forzar_ronda(ctx, codigo_torneo: str):
     """Se termina la rondar actual con empate de las partidas no jugadas y se inicia la siguiente - !forzar-ronda <código_torneo>"""
     await forzar_ronda_handle(ctx, codigo_torneo)
@@ -230,4 +191,5 @@ async def on_member_update(before, after):
 # webserver.keep_alive()  
 # bot.run(DISCORD_TOKEN)
 bot.run(config.TOKEN)
+
 
