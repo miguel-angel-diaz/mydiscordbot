a5e877b9ecb6178602d1b4da2a662201eedb0cb4
madiaz <madiaz@affin.es>
2026-08-05 22:43:52 +0200

    (fix): se realizan mejoras y nuevos desarrollos

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
 OPENROUTER_API_KEY =  os.environ.get("OPENROUTER_API_KEY")
 GUILD_ID_ADMISION = os.environ.get("GUILD_ID_ADMISION")
 CACHE_PATH = os.environ.get("CACHE_PATH")
-JWT_SECRET = os.environ.get("pepe")
+JWT_SECRET = os.environ.get("JWT_SECRET")
 
 if not CHALLONGE_USERNAME or not CHALLONGE_API_KEY or not DISCORD_TOKEN or not OPENROUTER_API_KEY or not JWT_SECRET:
     try:
